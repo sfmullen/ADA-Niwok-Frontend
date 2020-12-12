324fac6ce864d1981bc2a0cdf01eace66c40a893
Maria Hayward <hayward.maria@gmail.com>
2020-12-12 11:09:35 -0300

    traspaso componentes

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Header.scss';
+
+
+const Header = () => {
+    return (
+        <React.Fragment>
+            <main className="Main">
+                <header className="Main-header">
+                    <h2 className="header-title">AMAR LA TRAMA</h2>
+                </header>
+            </main>
+        </React.Fragment>
+    )
+};
+
+export default Header;
---
+++ src/Components/SearchView/SearchView.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './SearchView.scss';
+import Search from "../Search/Search";
+import Button from "../Button/Button";
+
+const SearchView = () => {
+    return (
+        <React.Fragment>
+            <Search />
+            <Button />
+        </React.Fragment>
+    )
+};
+
+export default SearchView;
