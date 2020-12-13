16fb37a67dd9b75cf14aa6f65afb0948c3b266d2
kimcitaw <kimcitaw@gmail.com>
2020-12-13 14:59:29 -0300

    lista de productos corregida

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -6,17 +6,38 @@ const Product = ({ title, foto, cantidad }) => {
         <React.Fragment>
             <div className="product-container">
                 <div className="product-details">
-                    <div className="product-figure">
-                        <img src={foto} className="img-product" />
-                    </div>
-                    <div className="decription-product">{title}</div>
                     <div className="quantity-product">
                             <p>Cantidad:{cantidad}</p> 
                     </div>
+                    <div className="product-figure">
+                        <img src={foto} className="img-product" />
+                    </div>
+                    
+                    <div className="decription-product">{title}</div>
                 </div>
             </div>
         </React.Fragment>
     )
 };
+
+// const Product = ({ title, foto, cantidad }) => {
+//     return (
+//         <React.Fragment>
+//             <div className="product-container">
+//                 <div className="product-details">
+//                     <div className="product-figure">
+//                         <img src={foto} className="img-product" />
+//                     </div>
+//                     <div className="quantity-product">
+//                             <p>Cantidad:{cantidad}</p> 
+//                     </div>
+//                     <div className="decription-product">{title}</div>
+//                 </div>
+//             </div>
+//         </React.Fragment>
+//     )
+// };
+
+
 
 export default Product;
